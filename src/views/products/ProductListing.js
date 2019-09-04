d8f53dedbdad243a72caeb5e2147fc3524a9d328
singuista <tensai213@gmail.com>
2019-09-04 17:35:46 -0400

    feat: changed site

--- src/views/products/ProductListing.js
+++ src/views/products/ProductListing.js
@@ -10,36 +10,82 @@ class ProductListing extends Component {
         const data = productData.default
 
         this.state = {
+			selectedProductData: {
+				price:  '0.01',
+				name: '',
+				label: '',
+				image: 'https://dummyimage.com/200x200/000/fff'
+			},
+			showModal: false,
             productData: {
                 data
             }
         }
-    }
+	}
 
+	/**
+	 * Convert markup string to JSX
+	 * @param {string} JSX formatted string
+	 * @return {html} 
+	**/
+	convertToJSX = str => {
+		return {__html: str}
+	}
+
+	/**
+	 * Event Handler to purchase
+	 * @param {number}  
+	 * @return {void}
+	**/
+	purchaseItem = (name, label, cost, event) => {
+		const price = (cost / 100) * 100
+
+		this.setState({
+			selectedProductData: {
+				price,
+				name, 
+				label,
+				image: 'https://dummyimage.com/200x200/000/fff'
+			}
+		}, () => {
+			this.showModal()
+		})
+	}
+
+	/**
+	 * Change the state to open the modal
+	 * @return {void}
+	**/
+	showModal = () => {
+		this.setState({showModal: true})
+	}
+
+	/**
+	 * Change the state to close the modal
+	 * @return {void}
+	**/
+	closeModal = () => {
+		this.setState({showModal: false})
+	}
+
+	/**
+	 * Take Product data that is state from the JSON files and output makeup around it
+	 * @return {jsx}
+	**/
     parseProductData() {
         const {productData} = this.state
 
-        //console.log(productData.data)
-
         let markup = productData.data.map((el, i) => {
-            const {name, description, price} = el
-
-            let priceMarkup = price.options.map((el, j) => {
-                const {label, cost} = el
-                return (
-                    <div key={`priceItem-${j}`}>
-                        <strong>{label}</strong>: ${parseFloat(cost, 2)}
-                    </div>
-                )
-            })
-
+			const {name, description, form} = el
+			
             return (
                 <div className="productItem" key={`productItem-${i}`}>
-                    <h2>{name}</h2>
-                    <p>{description}</p>
-                    <div className="productItem__pricingContainer">
-                        {priceMarkup}
-                    </div>
+					<aside className="productItem__image"><img src="https://dummyimage.com/240x4:3" /></aside>
+					<div className="productItem__info">
+						<h2 className="productItem__name">{name}</h2>
+						<p>{description}</p>
+					</div>
+                    <div className="productItem__pricingContainer" dangerouslySetInnerHTML={this.convertToJSX(form)} />
                 </div>
             )
         })
@@ -55,6 +101,7 @@ class ProductListing extends Component {
                 >
                     Shae Butter Products Listing
                 </Header>
+
                 <section className="productListingContainer viewPadding">
                     {this.parseProductData()}
                 </section>
