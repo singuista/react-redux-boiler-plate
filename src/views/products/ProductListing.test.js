fc86b7158ec1dc00ad1f6067f992adfdb3e46709
singuista <tensai213@gmail.com>
2019-09-05 19:19:10 -0400

    test: product listing test

--- src/views/products/ProductListing.test.js
+++ src/views/products/ProductListing.test.js
@@ -1,2 +1,11 @@
 import React from 'react'
-import { create } from "react-test-renderer";
+import { create } from "react-test-renderer"
+import ProductListing from './ProductListing'
+
+
+describe('ProductListing Component', () => {
+    test('renders without crashing', () => {
+        const view = create(<ProductListing />)
+        expect(view.toJSON()).toMatchSnapshot();
+    })
+})
