b14ab34231f431f3e6058e89a108caedbf80ee8c
singuista <tensai213@gmail.com>
2019-09-05 18:53:23 -0400

    chore: react router updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import Index from './views/index/Index'
-import NotFound from './views/404/NotFound'
-import './App.css'
+//Depenency imports
+import React 										from 'react'
+import { BrowserRouter, Router, Route, Switch } 	from 'react-router-dom'
+import {createBrowserHistory} 						from 'history'
+import { Provider } 								from 'react-redux';
+import configureStore 								from './store/Store'
 
+//Views for Routes
+import Index 										from './views/index/Index'
+import NotFound 									from './views/404/NotFound'
+
+//External dependencies 
+import './App.scss'
+
+const store = configureStore()
+const history = createBrowserHistory()
 
 function App() {
 	return (
 		<main className="App">
-			<header>This is the Header</header>
-
-			<Switch>
-				<Route exact path="/" component={Index} />
-				<Route path="*" component={NotFound}/>
-			</Switch>
+			<Provider store={store}>
+				<BrowserRouter>
+					<Router history={history}>
+						<Switch>
+							<Route exact path="/" component={Index} />
+							<Route path="*" component={NotFound}/>
+						</Switch>
+					</Router>
+				</BrowserRouter>
+			</Provider>
 		</main>
-	);
+	)
 }
 
-export default App;
+export default App
