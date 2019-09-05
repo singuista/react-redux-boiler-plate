707b1c3a7535c63d4ff5ecf8fe7bd7fcbf54a2d1
singuista <tensai213@gmail.com>
2019-09-05 18:50:32 -0400

    chore: updates to react router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React 						from 'react'
-import { Router, Route, Switch } 	from 'react-router-dom'
-import Index 						from './views/index/Index'
-import ProductListing 				from './views/products/ProductListing'
-import NotFound 					from './views/404/NotFound'
-import {createBrowserHistory} 		from 'history'
+//Depenency imports
+import React 										from 'react'
+import { BrowserRouter, Router, Route, Switch } 	from 'react-router-dom'
+import {createBrowserHistory} 						from 'history'
+import { Provider } 								from 'react-redux';
+import configureStore 								from './store/Store'
+
+//Views for Routes
+import Index 										from './views/index/Index'
+import ProductListing 								from './views/products/ProductListing'
+import NotFound 									from './views/404/NotFound'
+
+//External dependencies 
 import './App.scss'
 
-console.log(createBrowserHistory)
-
-const HISTORY = createBrowserHistory()
+const store = configureStore()
+const history = createBrowserHistory()
 
 function App() {
 	return (
 		<main className="App">
-			<Router history={HISTORY}>
-				<Switch>
-					<Route exact path="/" component={Index} />
-					<Route path="/products" component={ProductListing} />
-					<Route path="*" component={NotFound}/>
-				</Switch>
-			</Router>
+			<Provider store={store}>
+				<BrowserRouter>
+					<Router history={history}>
+						<Switch>
+							<Route exact path="/" component={Index} />
+							<Route path="/products" component={ProductListing} />
+							<Route path="*" component={NotFound}/>
+						</Switch>
+					</Router>
+				</BrowserRouter>
+			</Provider>
 		</main>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -9,14 +9,7 @@ import './index.css'
 
 const store = configureStore()
 
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>, 
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
